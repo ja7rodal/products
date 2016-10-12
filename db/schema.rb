80fd48d254227812f145daec3fb2ef784bb69dda
Jasson Rdz <ja7rodal@gmail.com>
2016-10-12 18:57:11 -0500

    fix join table v2

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,17 @@
 
 ActiveRecord::Schema.define(version: 20161012235104) do
 
-  create_table "Categories_Products", id: false, force: :cascade do |t|
-    t.integer "category_id", null: false
-    t.integer "product_id",  null: false
-    t.index ["category_id", "product_id"], name: "index_Categories_Products_on_category_id_and_product_id"
-    t.index ["product_id", "category_id"], name: "index_Categories_Products_on_product_id_and_category_id"
-  end
-
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "product_id",  null: false
+    t.index ["category_id", "product_id"], name: "index_categories_products_on_category_id_and_product_id"
+    t.index ["product_id", "category_id"], name: "index_categories_products_on_product_id_and_category_id"
   end
 
   create_table "products", force: :cascade do |t|
